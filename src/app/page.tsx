742617365d14122d6e8d6daf18c44dd9e2ada2dd
Tushar <tusharbajaj10@gmail.com>
2025-02-01 16:16:38 +0530

    correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { AAKAForm } from "@/components/Aakaform"
+import logo from '../../public/imgaaka.png'
 
 export default function Home() {
   return (
     <main className="container mx-auto py-10 bg-secondary min-h-screen">
-      <h1 className="text-3xl text-center mb-6 text-primary italic font-mono text-black font-thin ">AAKA</h1>
+      <div className="flex items-center justify-center gap-5 text-2xl sm:text-5xl text-center mb-6 text-primary italic font-sans text-black font-thin ">
+        <div>
+          <img className="w-10 sm:w-16" src={logo.src} alt="" />
+        </div>
+        <div>
+          Aaka-Official
+        </div>
+      </div>
       <AAKAForm />
     </main>
   )
