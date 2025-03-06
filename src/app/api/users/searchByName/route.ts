dcf509912999c8bdee68177ebd6b797c2ddcbe23
Tushar <tusharbajaj10@gmail.com>
2025-03-07 00:50:14 +0530

    changed search fxnality

--- src/app/api/users/searchByName/route.ts
+++ src/app/api/users/searchByName/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: Request) {
         const users = await prisma.user.findMany({
             where: {
                 fullName: {
-                    startsWith: trimmedNameValue,
+                    contains: trimmedNameValue,
                     mode: 'insensitive',
                 },
             },
